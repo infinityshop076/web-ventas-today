9c50d36ab4223ca552bad7375d65e88a9ff7be54
User <user@example.com>
2026-04-28 22:31:01 +0200

    fix: use automatic payment methods to resolve twint error

--- api/checkout.js
+++ api/checkout.js
@@ -34,7 +34,9 @@ export default async function handler(req, res) {
 
     // Create Checkout Session
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card', 'twint'],
+      automatic_payment_methods: {
+        enabled: true,
+      },
       shipping_address_collection: {
         allowed_countries: ['CH'], // Strictly Switzerland
       },
